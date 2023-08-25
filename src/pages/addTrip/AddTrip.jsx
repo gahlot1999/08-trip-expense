392870ba671a36ba8a7bc55f2026b830d89a4391
gahlot1999 <gahlot1999@gmail.com>
2023-08-26 00:40:59 +0530

    i

--- src/pages/addTrip/AddTrip.jsx
+++ src/pages/addTrip/AddTrip.jsx
@@ -1,17 +1,23 @@
 import styles from './AddTrip.module.css';
+import { IoMdArrowRoundBack } from 'react-icons/io';
 
 function AddTrip() {
   return (
-    <div className={styles.addTripForm}>
-      <form>
-        <div className={styles.fieldGroup}>
-          <input type='text' id='tName' placeholder='Trip Name' />
-          <label htmlFor='tName' className={styles.label}>
-            Trip Name
-          </label>
-        </div>
-      </form>
-    </div>
+    <>
+      <button className={styles.backBtn}>
+        <IoMdArrowRoundBack size={24} />
+      </button>
+      <div className={styles.addTripForm}>
+        <form>
+          <div className={styles.fieldGroup}>
+            <input type='text' id='tName' placeholder='Trip Name' />
+            <label htmlFor='tName' className={styles.label}>
+              Trip Name
+            </label>
+          </div>
+        </form>
+      </div>
+    </>
   );
 }
 
