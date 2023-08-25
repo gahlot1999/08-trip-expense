b61f9fcedfa4495beb7fc6bc8ab2f316e43e1ad1
gahlot1999 <gahlot1999@gmail.com>
2023-08-26 00:11:11 +0530

    i

--- src/pages/addTrip/AddTrip.jsx
+++ src/pages/addTrip/AddTrip.jsx
@@ -1,5 +1,18 @@
+import styles from './AddTrip.module.css';
+
 function AddTrip() {
-  return <div>AddTrip</div>;
+  return (
+    <div className={styles.addTripForm}>
+      <form>
+        <div className={styles.fieldGroup}>
+          <input type='text' id='tName' placeholder='Trip Name' />
+          <label htmlFor='tName' className={styles.label}>
+            Trip Name
+          </label>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default AddTrip;
