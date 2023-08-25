33e3f831df29422656a99aaf4787ea013d9f2cf7
gahlot1999 <gahlot1999@gmail.com>
2023-08-25 23:32:44 +0530

    i

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,20 @@
+import { useNavigate } from 'react-router-dom';
 import styles from './Home.module.css';
 
 function Home() {
+  const navigate = useNavigate();
+  function handleNewTrip() {
+    navigate('/add');
+  }
+
+  function handleExistingTrip() {
+    navigate('/trips');
+  }
+
   return (
     <div className={styles.home}>
-      <button>New Trip?</button>
-      <button>Existing Trip?</button>
+      <button onClick={handleNewTrip}>New Trip?</button>
+      <button onClick={handleExistingTrip}>Existing Trip?</button>
     </div>
   );
 }
