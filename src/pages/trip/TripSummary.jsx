275ba40970d8d47234f4a6b061dd60b8bd0dfc43
gahlot1999 <gahlot1999@gmail.com>
2023-09-01 21:36:41 +0530

    added summary

--- src/pages/trip/TripSummary.jsx
+++ src/pages/trip/TripSummary.jsx
@@ -1,8 +1,26 @@
-import { formatPrice } from '../../services/helpers';
+import {
+  Cell,
+  Legend,
+  Pie,
+  PieChart,
+  ResponsiveContainer,
+  Tooltip,
+} from 'recharts';
+import { formatPrice, randomColor } from '../../services/helpers';
 import styles from './TripSummary.module.css';
 
+/* const tripSummaryByCategory1 = [
+    { category: 'Accommodation', amount: 4500, color: 'red' },
+    { category: 'Food', amount: 2000, color: 'royalblue' },
+    { category: 'Travel', amount: 1500, color: 'orangered' },
+    { category: 'Entertainment', amount: 1000, color: 'green' },
+    { category: 'Other', amount: 700, color: 'gray' },
+  ]; */
+
 function TripSummary({ expenseData }) {
-  const tripSummary = {};
+  const tripSummary = [];
+  const dummyTripSummaryByCategory = [];
+  const tripSummaryByCategory = [];
 
   expenseData?.forEach((expense) => {
     const { paidBy, amount } = expense;
@@ -14,17 +32,60 @@ function TripSummary({ expenseData }) {
     }
   });
 
+  expenseData?.forEach((expense) => {
+    let { category, amount } = expense;
+
+    if (!dummyTripSummaryByCategory[category]) {
+      dummyTripSummaryByCategory[category] = amount;
+    } else {
+      dummyTripSummaryByCategory[category] += amount;
+    }
+  });
+
+  for (const category in dummyTripSummaryByCategory) {
+    tripSummaryByCategory.push({
+      category,
+      amount: dummyTripSummaryByCategory[category],
+      color: randomColor(),
+    });
+  }
+
+  randomColor();
+
   return (
     <div className={styles.summary}>
       <h2>Trip Summary</h2>
 
-      <div className={styles.summaryChart}>Chart</div>
+      <div className={styles.summaryChart}>
+        <ResponsiveContainer width='100%' height={250}>
+          <PieChart>
+            <Pie
+              data={tripSummaryByCategory}
+              nameKey='category'
+              dataKey='amount'
+              innerRadius={75}
+              outerRadius={100}
+              paddingAngle={3}
+            >
+              {tripSummaryByCategory.map((entry) => (
+                <Cell
+                  fill={entry.color}
+                  stroke={entry.color}
+                  key={entry.color}
+                />
+              ))}
+            </Pie>
+            <Tooltip />
+            <Legend iconSize={10} iconType='circle' />
+          </PieChart>
+        </ResponsiveContainer>
+      </div>
 
       <div className={styles.expenseSummaryContainer}>
         {Object.entries(tripSummary).map((el) => (
           <div key={el} className={styles.expenseSummary}>
-            <p>{el[0]}</p>
-            <p>{formatPrice(el[1])}</p>
+            <p>{el.at(0)}</p>
+            <p>{formatPrice(el.at(1))}</p>
           </div>
         ))}
       </div>
--- src/services/helpers.js
+++ src/services/helpers.js
@@ -12,3 +12,12 @@ export function formatPrice(price) {
 
   return formattedPrice;
 }
+
+// To get random hsl color
+export function randomColor() {
+  const random1 = (Math.random() * 100).toFixed(2);
+  const random2 = (Math.random() * 100).toFixed(2);
+  const random3 = (Math.random() * 70).toFixed(2);
+
+  return `hsl(${random1}, ${random2}%, ${random3}%)`;
+}
